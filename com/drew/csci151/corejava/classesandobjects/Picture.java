be77dbd0fd83ec34b7adfc4789cff34bc812ac63
Ordway <Ordway.Nicholas@gmail.com>
2024-02-02 11:08:28 -0500

    Updated Picture Class

--- com/drew/csci151/corejava/classesandobjects/Picture.java
+++ com/drew/csci151/corejava/classesandobjects/Picture.java
@@ -2,15 +2,15 @@ package com.drew.csci151.corejava.classesandobjects;
 
 //Class
 public class Picture {
-	
+
 	// Attributes
 	private int length;
 	private int width;
 	private double contrast;
 	private String title;
-	
+
 	// Constructor
-		// Default
+	// Default
 	public Picture() {
 		super();
 		this.length = -1;
@@ -18,67 +18,98 @@ public class Picture {
 		this.contrast = -1;
 		this.title = "";
 	}
-	
-		// With Arguments
+
+	// With Arguments
 	public Picture(int length, int width, double contrast, String title) {
 		super();
+		// if we wanted to construct pictures only lager than 10 otherwise set default 10
+//		if (length < 10) {
+//			this.length = 10;
+//		} else {
+//			this.length = length;
+//		}
 		this.length = length;
 		this.width = width;
 		this.contrast = contrast;
 		this.title = title;
 	}
-	
+
+	public Picture(int length, int width, double contrast) {
+		super();
+		this.length = length;
+		this.width = width;
+		this.contrast = contrast;
+
+	}
+
 	// Methods
-		//Getters and Setters
-	
+
+	@Override
+	public String toString() {
+		return "Picture [length=" + length + ", width=" + width + ", contrast=" + contrast + ", title=" + title + "]";
+	}
+
+	// Method to be overridden later
+	public void move() {
+		System.out.println("The animal moves.");
+	}
+
+	// Getters and Setters
+
 	/**
 	 * @return the length
 	 */
 	public int getLength() {
 		return length;
 	}
+
 	/**
 	 * @param length the length to set
 	 */
 	public void setLength(int length) {
 		this.length = length;
 	}
+
 	/**
 	 * @return the width
 	 */
 	public int getWidth() {
 		return width;
 	}
+
 	/**
 	 * @param width the width to set
 	 */
 	public void setWidth(int width) {
 		this.width = width;
 	}
+
 	/**
 	 * @return the contrast
 	 */
 	public double getContrast() {
 		return contrast;
 	}
+
 	/**
 	 * @param contrast the contrast to set
 	 */
 	public void setContrast(double contrast) {
 		this.contrast = contrast;
 	}
+
 	/**
 	 * @return the title
 	 */
 	public String getTitle() {
 		return title;
 	}
+
 	/**
 	 * @param title the title to set
 	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
 
 }
--- com/drew/csci151/corejava/classesandobjects/PictureRunner.java
+++ com/drew/csci151/corejava/classesandobjects/PictureRunner.java
@@ -4,18 +4,24 @@ package com.drew.csci151.corejava.classesandobjects;
 public class PictureRunner {
 
 	public static void main(String[] args) {
-		
+
 		// Class Declaration
 		Picture art;
-		
-			
+
 		// Object Initialization & show constructors
 		art = new Picture();
-		Picture art2 = new Picture();		
-		// Show messages and methods
-			
-		// Use getters and toString to print to console
+		Picture art2 = new Picture(7, 8, 2.3, "Good Art Pic");
+		Picture art3 = new Picture(2, 3, 1.3);
 		
+		// Show messages and methods
+		art.setLength(8);		
+		art.setContrast(5.5);
+		
+		// Use getters and toString to print to console
+		System.out.println(art.getLength());
+		System.out.println(art2.getLength());
+		System.out.println(art3.getLength());
+		System.out.println(art.toString());
 
 	}
 
